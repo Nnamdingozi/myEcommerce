e22257fe6a8763da0c9ba8f08b0e89fc2152efa5
joerald <ngozika1105@gmail.com>
2024-11-08 21:12:09 +0100

    modify category page code

--- frontend/myecommerce/app/category/[id]/page.tsx
+++ frontend/myecommerce/app/category/[id]/page.tsx
@@ -1,52 +1,54 @@
-'use client'
+"use client"; // Assuming the component is intended to run on the client side
 
 import { useEffect, useState } from 'react';
 import { Product } from '@/app/lib/definition';
 import { fetchProductsByCategoryId } from '@/app/lib/data';
-import  CategoryProducts  from '@/app/ui/categoryProducts';
+import CategoryProducts from '@/app/ui/categoryProducts';
 import { useCart } from '@/app/context/cartContext';
 
-export default function CategoryPage({params}: { params : {id: string}}) {
-
-    const [catProducts, setCatProducts] = useState<Product[]>([]);
-    const [loading, setLoading] =  useState<boolean>(true);
-const [error, setError] = useState<string | null>(null);
-
-const {addToCart, getUserCart} = useCart();
-
-
-    useEffect(() => {
-        const baseUrl = 'http://localhost:5000';
-        const fetchByCat = async () => {
-            try {
-                const categoryproducts: Product[] = await fetchProductsByCategoryId(params.id);
-    console.log('category Products fetched:', categoryproducts);
-
-   
-    const modfifiedCategoryproducts = categoryproducts.map((categoryproduct) => ({
-        ...categoryproduct, 
-        image_url: `${baseUrl}${categoryproduct.image_url}`
-    }));
-    setCatProducts(modfifiedCategoryproducts)
-
-            } catch (err) {
-                console.error('Error fetching products for this category');
-                setError('Failed to fetch products for this product')
-            } finally {
-                setLoading(false)
-            }
-        }
-fetchByCat()
-    }, [params.id])
-    
-    return (
-        <CategoryProducts 
-        categoryproducts={catProducts}
-        addToCart={addToCart}
-        getUserCart={getUserCart}
-         />
-    )
+interface CategoryPageProps {
+  params: { id: string };
 }
 
+export default function CategoryPage({ params }: CategoryPageProps) {
+  const [catProducts, setCatProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
+  const { addToCart, getUserCart } = useCart();
 
+  useEffect(() => {
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+    const fetchByCat = async () => {
+      try {
+        const categoryproducts: Product[] = await fetchProductsByCategoryId(params.id);
+        console.log('category Products fetched:', categoryproducts);
+
+        const modifiedCategoryProducts = categoryproducts.map((categoryproduct) => ({
+          ...categoryproduct,
+          image_url: `${backendUrl}${categoryproduct.image_url}`
+        }));
+
+        setCatProducts(modifiedCategoryProducts);
+      } catch (err) {
+        console.error('Error fetching products for this category', err);
+        setError('Failed to fetch products for this category');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchByCat();
+  }, [params.id]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <CategoryProducts
+      categoryproducts={catProducts}
+      addToCart={addToCart}
+      getUserCart={getUserCart}
+    />
+  );
+}
