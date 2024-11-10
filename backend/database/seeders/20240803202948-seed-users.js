1496dcb457b53fca71d42953890a2d6b1d5bb6ad
joerald <ngozika1105@gmail.com>
2024-11-10 10:44:07 +0100

    modify seeder files

--- backend/database/seeders/20240803202948-seed-users.js
+++ backend/database/seeders/20240803202948-seed-users.js
@@ -4,26 +4,35 @@ const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const hashedPassword = await bcrypt.hash('Example1@pw', 10); // Hash the password
+    // Check if the users table already contains data
+    const existingUsers = await queryInterface.sequelize.query(
+      `SELECT * FROM users WHERE username = 'joerald' AND email = 'example@gmail.com';`
+    );
 
-    await queryInterface.bulkInsert('users', [
-      {
-        username: 'joerald',
-        email: 'example@gmail.com',
-        phone: '090090090000',
-        password: hashedPassword, // Use the hashed password
-        githubId: null,
-        country_code: '+234',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      // Add more users as needed
-    ], {});
+    // Proceed with seeding only if no matching user is found
+    if (existingUsers[0].length === 0) {
+      const hashedPassword = await bcrypt.hash('Example1@pw', 10); // Hash the password
+
+      await queryInterface.bulkInsert('users', [
+        {
+          username: 'joerald',
+          email: 'example@gmail.com',
+          phone: '090090090000',
+          password: hashedPassword, // Use the hashed password
+          githubId: null,
+          country_code: '+234',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        // Add more users as needed
+      ], {});
+    } else {
+      console.log('Seed data already exists in "users" table. Skipping seeding.');
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('users', null, {});
     await queryInterface.sequelize.query('ALTER SEQUENCE "users_id_seq" RESTART WITH 1;');
-    
   }
 };
