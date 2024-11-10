74f20564a4e877ac8ea46822617e8b639a55d375
joerald <ngozika1105@gmail.com>
2024-11-10 23:21:22 +0100

    modify userContext to store session in cookies

--- frontend/myecommerce/next.config.mjs
+++ frontend/myecommerce/next.config.mjs
@@ -1,38 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/home',
-          permanent: true, // Set this to true or false based on your redirect preference
-        },
-      ];
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: process.env.NODE_ENV === 'production' 
-            ? 'myecommerce-la62.onrender.com'
-            : 'localhost',
-          pathname: '/images/*',
-        },
-      ],
-    },
-    
-  };
-  
-  export default nextConfig;
-  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // Set this to true or false based on your redirect preference
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',
+        hostname: new URL(process.env.NEXT_PUBLIC_BACKEND_URL).hostname,
+        pathname: '/images/*',
+      },
+    ],
+  },
+};
 
-
-
+export default nextConfig;
