d3fc77f4f2dcfaf8375936b05f09e7ce4306b2c1
joerald <ngozika1105@gmail.com>
2024-12-01 11:24:46 +0100

    adjust footer margin

--- frontend/myecommerce/app/ui/footer.tsx
+++ frontend/myecommerce/app/ui/footer.tsx
@@ -3,7 +3,7 @@
 
  export default function Footer () {
     return (
-<div className="w-[95%] bg-gray-300 mx-2 text-red-800 shadow-lg flex flex-col lg:flex-row justify-between items-center px-4 h-64 text-lg font-bold ">
+<div className="w-[98%] bg-gray-300  text-red-800 shadow-lg flex flex-col lg:flex-row justify-between items-center px-4 h-64 text-lg font-bold mx-auto">
     <div className='hover:text-blue-700 cursor-pointer underline'><Link href={'/contacts'}>Contact us</Link></div>
     <div className='hover:text-blue-700 cursor-pointer underline'><Link href={'/aboutUs'}>About Us</Link></div>
     <div className='hover:text-blue-700 cursor-pointer underline'><Link href={'/sell'}>Sell on Family Shop</Link></div>
